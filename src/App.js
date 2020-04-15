359cfd6e4480685d5b2575f0e53241e56287ffdd
wolv66 <wolv66@users.noreply.github.com>
2020-04-15 15:05:08 -0700

    add some css and made everything beautiful

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+
+    let months = ['January', 'February', 'March', "April", 'May', 'June', 'July',
+      'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
+
   return (
     <div className="app morning">
       <main>
@@ -18,8 +35,21 @@ function App() {
             placeholder='Search...'
 
           />
+        </div>
+        <div className='location-container'>
+          <div className='location'> Vancouver, Canada </div>
+          <div className='date'>{dateBuilder(new Date())}</div>
+        </div>
+        <div className='weather-container'>
+          <div className='temp'>
+            15° c
+          </div>
+          <div className='weather'>
+            Sunny
+          </div>
 
         </div>
+        
       </main>
 
     </div>
