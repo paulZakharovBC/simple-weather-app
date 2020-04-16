dfd936a9c303b96c4f351795d43a81647f76bd07
Wolv66 <wolv66@gmail.com>
2020-04-15 17:22:53 -0700

    Updates

---
+++ precache-manifest.3c0e0c4c57b35ee771233da0cf7f2fc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1903fd0435b5f110d7398fb745c02e0e",
+    "url": "/simple-weather-app/index.html"
+  },
+  {
+    "revision": "88c6cee4ad9730791f8f",
+    "url": "/simple-weather-app/static/css/main.22862d2b.chunk.css"
+  },
+  {
+    "revision": "6e49229ea53aa2caeed7",
+    "url": "/simple-weather-app/static/js/2.18a60309.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-weather-app/static/js/2.18a60309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c6cee4ad9730791f8f",
+    "url": "/simple-weather-app/static/js/main.892ac3c0.chunk.js"
+  },
+  {
+    "revision": "67b2802be049f7060683",
+    "url": "/simple-weather-app/static/js/runtime-main.1283a28a.js"
+  },
+  {
+    "revision": "89d4cbd8fc07a4e83a4f5dd30b7a9b44",
+    "url": "/simple-weather-app/static/media/morning-bg.89d4cbd8.jpg"
+  }
+]);
